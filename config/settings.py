f337b88bbc55578d256a5a4171f5d5fe7028bcac
Reznov Ammar <reznov110@gmail.com>
2021-09-26 13:04:26 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -7,8 +7,8 @@ SEARCH_URL = f"{MAIN_URL}/rest/api/3/search"
 
 CONFIG = {
   "key_file": os.environ.get('PVT_FILE', "jira_privatekey.pcks8"),
-  "access_token": os.environ.get('ACCESS_TOKEN', "Mq2mmRgcQrzechQJNtEuiQHc181fNFQf"),
-  "secret_token": os.environ.get('SECRET_TOKEN', "3yKd2IJz400oGMb8zeR2FkmR7yF1cEX5"),
+  "access_token": os.environ.get('ACCESS_TOKEN', "hqZXIowUGG5UOSW1EEjJc5kooDP8XJYe"),
+  "secret_token": os.environ.get('SECRET_TOKEN', "HBGpmwwr2RlVUuw3vNUFHTISzMLMtEcm"),
   "consumer_key": f'{os.environ.get("JIRA_CON_KEY", "OauthKey")}',
   "project_key": f'{os.environ.get("JIRA_PROJECT_KEY", "MYB")}',
 }
