2c54832b2ba24f85b52e2e4ae78270f8192a7131
Reznov Ammar <reznov110@gmail.com>
2021-09-26 13:04:43 +0300

    Update jira_module.py

--- jira_module.py
+++ jira_module.py
@@ -1,82 +1,57 @@
-import requests
+from types import FunctionType
 import dateutil.parser as dtime
+from config.settings import CONFIG as config
 
 
 class JiraTest():
+  
+  # We can inherit JIRA module into class
+  # but for the purpose of simplicity I won't do it
 
-  def __init__(self, domain, search_url, config) -> None:
-    self.domain = domain
-    self.search_url = search_url
-    self.config = config
-
-
-  def get_issues(self) -> dict:
-    # Ечли запрос отправить через прокси 
-    # то нужно указать параметры proxy в конфигурации
-    response = requests.get(
-      self.search_url,
-      headers=self.config['headers'],
-      params=self.config['query'] ,
-      auth=self.config['auth_params'],
-      proxies=self.config['proxy']
-    )
-    data = response.json()
-    return data
+  def __init__(self, auth_jira) -> None:
+    self.auth_jira = auth_jira
 
 
-  def start(self) -> None:
+  def get_issues(self) -> None:
 
-    print("\nВытащим информацию для Вас!!...\n")
+    print("\nGetting tickets...\n")
 
-    issues_data = self.get_issues()
-    issues = issues_data["issues"]
+    issues_size = 100
+    issues_initial_index = 0
 
-    for idx, issue in enumerate(issues):
-      issue_key = issue["key"]
-      issue_url =  f"{self.domain}/rest/api/3/issue/{issue_key}"
-      response = requests.get(
-        issue_url,
-        headers=self.config['headers'],
-        auth=self.config['auth_params']
-      )
-      data = response.json()
+    while True:
+        
+      start= issues_initial_index * issues_size
+      issues = self.auth_jira.search_issues(f'project={config["project_key"]}', start, issues_size)
+      
+      if len(issues) == 0:
+        break
+      
+      issues_initial_index += 1
+      
+      for issue in issues:
+        
+        created_dtime = self.dtime_formater(issue.fields.created)
 
-      print(
-        f'ID Проблема ({issue_key}) со статусом ({data["fields"]["status"]["name"]})\n'
-      )
+        content = f'Ticket number = {issue}\n'
+        content += f'Creator = {issue.fields.creator}\n'
+        content += f'IssueType = {issue.fields.issuetype.name}\n'
+        content += f'Status = {issue.fields.status.name}\n'
+        content += f'Summary = {issue.fields.summary}\n'
+        content += f'Description = {issue.fields.description}\n'
+        content += f'Votes = {issue.fields.votes.votes}\n'
+        content += f'Created = {created_dtime.strftime("%d-%m-%Y %H:%M:%S")}\n'
+        content += f'========================================\n'
 
-      content = f'Ссылка проблемы: {issues[idx]["self"]}\n'
-      content += f'Ключ проблемы: {issues[idx]["key"]}\n'
-      content += f'Ссылка тип проблемы: {issues[idx]["fields"]["issuetype"]["self"]}\n'
-      content += f'Название тип проблемы: {issues[idx]["fields"]["issuetype"]["name"]}\n'
-      content += f'Описаие проблемы: {issues[idx]["fields"]["issuetype"]["description"]}\n'
-      content += f'Иконка: {issues[idx]["fields"]["issuetype"]["iconUrl"]}\n'
-      content += f'Ссылка на проект: {issues[idx]["fields"]["project"]["self"]}\n'
-      content += f'Название проекта: {issues[idx]["fields"]["project"]["name"]}\n'
-      content += f'Аватарка проекта: {issues[idx]["fields"]["project"]["avatarUrls"]["48x48"]}\n'
-      content += f'Резюме проекта: {issues[idx]["fields"]["summary"]}\n'
-      content += f'Создатель: {issues[idx]["fields"]["creator"]["displayName"]}\n'
-      content += f'Создатель email: {issues[idx]["fields"]["creator"]["emailAddress"]}\n'
-      content += f'Создатель аватарка: {issues[idx]["fields"]["creator"]["avatarUrls"]["48x48"]}\n'
-      content += f'Ссылка создателя: {issues[idx]["fields"]["creator"]["self"]}\n'
-      content += f'Голосов: {issues[idx]["fields"]["votes"]["votes"]}\n'
-
-      created_dtime = issues[idx]["fields"]["created"]
-      last_view_dtime = issues[idx]["fields"]["lastViewed"]
-
-      created_dtime_format = self.dtime_formater(created_dtime)
-      last_view_dtime_format = self.dtime_formater(last_view_dtime)
-
-      content += f'Создано: {created_dtime_format.strftime("%d-%m-%Y %H:%M:%S")}\n'
-      content += f'Последний ревью: {last_view_dtime_format.strftime("%d-%m-%Y %H:%M:%S")}\n'
-      content += f'===================================\n\n\n'
-
-      self.write_to_file(content)
-      self.print_content(content)
+        self.write_to_file(content)
+        self.print_content(content)
 
 
   def dtime_formater(self, date) -> str:
     return dtime.parse(date)
+
+  def print_content(self, content) -> None:
+    print(content)
 
   
   def write_to_file(self, content) -> None:
@@ -84,6 +59,14 @@ class JiraTest():
       fi.write(content)
       fi.close()
 
+  def create_new_issue(self, data=None) -> FunctionType:
+    if not data:
+      print('Please, provide ticket data')
+      return
 
-  def print_content(self, content) -> None:
-    print(content)
+    issues = self.auth_jira.create_issues(field_list=data)
+
+    if issues:
+      print("Ticket created successfully.")
+
+    return self.get_issues()
